3780ca182b63fc2bbc6ff374d123c6f199d98039
Jason <hjwouden@gmail.com>
2014-04-22 11:49:40 -0600

    Transferring to view
    moving parts relevant to the view to view. adjusting model

--- Scripts/model.js
+++ Scripts/model.js
@@ -1,5 +1,4 @@
 //Checkers V1.0 ---model.js
-
 //model.js for keeping track of checkers game board state
 
 //Variables used in arrays / (validate move)
@@ -9,10 +8,8 @@
 //2 = White Piece
 //4 = White Piece King
 
-//var Num_Players; //1 or 2 player game
-//var Player turn = 1; //== 1 or 2 for reds or whites, allows you to click highlight those pieces and move them.
-
-//somewhere move piece function needs to check if the piece is on the back row to make it a king,
+//////////////////////////////////////////////////////////////
+////////////////Board Arrays//////////////////////////////////
 
 //Array that is the layout of Starting Game
 var StartBoard = [0,1,0,1,0,1,0,1,
@@ -35,17 +32,6 @@ var ValidMove = [0,1,0,1,0,1,0,1,
                  0,1,0,1,0,1,0,1,
                  1,0,1,0,1,0,1,0,];
 
-//Sample of Game State, for showing
-//draw class, delete in final//
-var SampleGameState =   [0,4,0,1,0,1,0,1,
-                         1,0,2,0,1,0,1,0,
-                         0,0,0,0,0,1,0,1,
-                         0,0,0,0,0,0,0,0,
-                         0,0,0,0,0,0,0,0,
-                         1,0,0,0,0,0,2,0,
-                         0,0,0,0,0,0,0,2,
-                         3,0,0,0,0,0,2,0];
-
 //Array shows the current Game State
 var CurrentGame = [0,1,0,1,0,1,0,1,
                    1,0,1,0,1,0,1,0,
@@ -55,6 +41,7 @@ var CurrentGame = [0,1,0,1,0,1,0,1,
                    2,0,2,0,2,0,2,0,
                    0,2,0,2,0,2,0,2,
                    2,0,2,0,2,0,2,0];
+
 
 //Game Board for Outnumbered Variation
 var outNumberedBoard = [0,1,0,1,0,1,0,1,
@@ -76,23 +63,22 @@ var KingsBattleBoard = [0,3,0,3,0,3,0,3,
                         0,4,0,4,0,4,0,4,
                         4,0,4,0,4,0,4,0];
 
-
-//during testing using sample game state
-
+////////////////End Board Arrays///////////////////////////////
+///////////////////////////////////////////////////////////////
 
 var gameboard = document.getElementById("gameboard");
-//newGame();
-drawCurrentGameBoard(SampleGameState);
-AnimateTitle();
+newGame();
 
-CurrentGame = SampleGameState;
+AnimateTitle(); // move to the view
+
+CurrentGame = StartBoard;
+
 //function draws the starting Gameboard
 // this function should call somehting in the the view because it sets the view up. 
-function newGame(){
+function newGame(){  //
     var GameType = document.getElementById("gameType");
     var TypeVal = GameType.options[GameType.selectedIndex].value;
-    //alert(TypeVal);
-    
+    GameHistoryUpdate("Starting New Game");
     if (TypeVal == "0") {
         //normal game board
     var k = 0;
@@ -111,21 +97,19 @@ function newGame(){
     CurrentGame = StartBoard;
     
     }//end if TypeVal == 0 normal game board
-    else if (TypeVal == "1") {
-        //outnumbered game board
+    else if (TypeVal == "1") { //outnumbered game board
         drawCurrentGameBoard(outNumberedBoard);
         CurrentGame = outNumberedBoard;
     }//end if TypeVal == 1 outnumbered battle
-    else if (TypeVal == "2") {
-        //Kings Battle Game board
+    else if (TypeVal == "2") { //Kings Battle Game board
         drawCurrentGameBoard(KingsBattleBoard);
         CurrentGame = KingsBattleBoard;
     }//end if TypeVal == 2 kings battle
-    GameHistoryDebug();
+    GameHistoryUpdate("White goes first!");
 }
 
 
-//adding event handlers to table cells
+//adding event handlers to table cells.
 var cells = document.getElementsByTagName("td");
 var i = 0;
 for(i = 0; i<64; i++)
@@ -135,44 +119,72 @@ for(i = 0; i<64; i++)
     var row = this.parentNode.rowIndex;
     squareClicked(row, col);
    }
-} 
+}
+
+//variables for game.
 var PlayerTurn = 0; // 0 for white, 1 for red
 var lastclickedPiece = -1;
 var lastclickedPosition = -1;
 var PieceHighlighted = 0; // if a piece is highlighted will be 1, else 0 meaning nothing is selected;
 var HighlightedPieceNum = -1; // the number of the piece that is highlighted, so we can refer back to it, -1 when no piece is selected;
+var TotalRedPieces = 12;
+var TotalWhitePieces = 12;
 
-function ChangeTurn(){
+function ChangeTurn(){ // rotate turns each
     if (PlayerTurn == 0) {
         PlayerTurn = 1;
+        GameHistoryUpdate("Reds Turn"); // make it for only a certain <p> tag
     }
     else{
         PlayerTurn = 0;
+        GameHistoryUpdate("Whites Turn");
     }
+    // should write in the game log whos turn it is.
 }
-function MovePiece(from, to){
-    // move the piece.
+
+function CheckKingMe(){ // check to see if anyone needs to be kinged
+    for(var i = 0; i<8; i++)
+    {
+        if (CurrentGame[i] == 2) {
+            CurrentGame[i] = 4;
+            //display a king me message
+        }
+    }
+    for(var i = 56; i<65; i++)
+    {
+        if (CurrentGame[i] == 1) {
+            CurrentGame[i] = 3;
+            //display a king me message
+        }
+    }
+}//end CheckKingMe();
+
+
+
+function MovePiece(from, to){ // move the piece.
     CurrentGame[to] = lastclickedPiece;
     CurrentGame[from] = 0;
     //reset the variables
     PieceHighlighted = 0;
     lastclickedPiece = -1;
     lastclickedPosition = -1;
+    
+    CheckKingMe();
     //Draw the new board
     drawCurrentGameBoard(CurrentGame);
-    changeTurn();
-}
-function InvalidMove(){
-    //move was not valid, reset variables and re-draw board
-   // alert("invalid move");
+    ChangeTurn();
+
+}//end MovePiece Function
+
+function InvalidMove(){ //move was not valid, reset variables and re-draw board
     lastclickedPiece = -1;
     lastclickedPosition = -1;
     PieceHighlighted = 0;
     drawCurrentGameBoard(CurrentGame);
-}
+    GameHistoryUpdate("Invalid Move!");
+} // end InvalidMove();
 
-// Click Event Handler for Checkerboard. Takes the row and col in the table that was clicked.
-function squareClicked(row, col){
+function squareClicked(row, col){ // Click Eventhandler for checkerboard,
     var converted = (row * 8) + col;
 
     if (PieceHighlighted == 1) {  // a piece is selected, check if moving, jumping, or highlight new piece
@@ -198,9 +210,7 @@ function squareClicked(row, col){
                         }
                         else{InvalidMove();}
                     }
-                    else{ // was not a jump, or next to us, invalid move.
-                    InvalidMove(); // invalid move, pass invalid move string of who we are for better feedback in console
-                    }
+                    else{InvalidMove();}
                 }
             }//endif Red Piece.
             
@@ -224,9 +234,7 @@ function squareClicked(row, col){
                         }
                         else{InvalidMove();}
                     }
-                    else{ //was not a jump, or next to us, invalid move.
-                    InvalidMove();
-                    }
+                    else{InvalidMove();}
                 }
             }//endif White Piece
             
@@ -263,9 +271,7 @@ function squareClicked(row, col){
                         }
                         else{InvalidMove();}
                     }
-                    else{ // was not a jump, or next to us, invalid move
-                    InvalidMove();
-                    }
+                    else{InvalidMove();}
                 }
            } //endif Red King Piece
            
@@ -302,79 +308,86 @@ function squareClicked(row, col){
                         }
                         else{InvalidMove();}
                     }
-                    else{
-                    InvalidMove();
-                    }
+                    else{InvalidMove();}
                 }
            }
         } //endif White King Piece
-        
-        else{ //some other reason
-            InvalidMove();
-        }
-        
+        else{InvalidMove();}   
     } //endif A piece was already highlighted.
-
-
-    else { // a piece is not highlighted, highlight a piece you want to move.
-        
-    if (ValidMove[converted] == 0) {
-        //invalid move, do nothing, clicked either white or empty square
-    //    alert("clicked white square = " + converted );
-    }
-
-// first click on a piece, find out which piece and make it look highlighted.
-    else if (CurrentGame[converted] == 1) {
-        //clicked on a red piece, highlight if first click
-        var cell = gameboard.rows[row].cells[col];
-        cell.className="redPCclicked";
-        PieceHighlighted = 1;
-        GameHistory(row, col);
-        //may first want to make sure that the last clicked var is free.
-        lastclickedPiece = CurrentGame[converted];
-        lastclickedPosition = converted;
+    
+    else{ // a piece is not highlighted, highlight a piece you want to move.
+        if (ValidMove[converted] == 0) { // do nothing, clicked on a white or empty square
         }
-
-
-    else if (CurrentGame[converted] == 2) {
-        //clicked on a white piece, highlight if first click
-        var cell = gameboard.rows[row].cells[col];
-        cell.className="whitePCclicked";
-        PieceHighlighted = 1;
-        GameHistory(row, col);
-        lastclickedPiece = CurrentGame[converted];
-        lastclickedPosition = converted;
-    }
-
-
-    else if (CurrentGame[converted] == 3) {
-        //clicked on a white piece, highlight if first click
-        var cell = gameboard.rows[row].cells[col];
-        cell.className="redPCKingclicked";
-        PieceHighlighted = 1;
-        GameHistory(row, col);
-        lastclickedPiece = CurrentGame[converted];
-        lastclickedPosition = converted;
-    }
-
-
-    else if (CurrentGame[converted] == 4) {
-        //clicked on a white piece, highlight if first click
-        var cell = gameboard.rows[row].cells[col];
-        cell.className="whitePCKingclicked";
-        PieceHighlighted = 1;
-        GameHistory(row, col);
-        lastclickedPiece = CurrentGame[converted];
-        lastclickedPosition = converted;
-    }
-   } // end else
-}
+        else if (CurrentGame[converted] == 1 && PlayerTurn == 1) { // first click on a piece, find out which piece and make it look highlighted.
+            //highlighting piece should be part of the view, call the function to do it over there.
+            //clicked on a red piece, highlight if first click
+            var cell = gameboard.rows[row].cells[col];
+            cell.className="redPCclicked";
+            PieceHighlighted = 1;
+            GameHistory(row, col);
+            //may first want to make sure that the last clicked var is free.
+            lastclickedPiece = CurrentGame[converted];
+            lastclickedPosition = converted;
+        }
+        else if (CurrentGame[converted] == 2 && PlayerTurn == 0) {
+            //clicked on a white piece, highlight if first click
+            var cell = gameboard.rows[row].cells[col];
+            cell.className="whitePCclicked";
+            PieceHighlighted = 1;
+            GameHistory(row, col);
+            lastclickedPiece = CurrentGame[converted];
+            lastclickedPosition = converted;
+        }
+        else if (CurrentGame[converted] == 3 && PlayerTurn == 1) {
+            //clicked on a white piece, highlight if first click
+            var cell = gameboard.rows[row].cells[col];
+            cell.className="redPCKingclicked";
+            PieceHighlighted = 1;
+            GameHistory(row, col);
+            lastclickedPiece = CurrentGame[converted];
+            lastclickedPosition = converted;
+        }
+        else if (CurrentGame[converted] == 4 && PlayerTurn == 0) {
+            //clicked on a white piece, highlight if first click
+            var cell = gameboard.rows[row].cells[col];
+            cell.className="whitePCKingclicked";
+            PieceHighlighted = 1;
+            GameHistory(row, col);
+            lastclickedPiece = CurrentGame[converted];
+            lastclickedPosition = converted;
+        }
+    } // end else
+} // end the Click Event Handler.
 
 
 function JumpedPiece(position){
-    CurrentGame[position] = 0;
-    // will also decrement the total number of pieces and check to see if the game is over. record the lost piece in game history.
+    if (CurrentGame[position] == 1 || CurrentGame[position] == 3) {
+        CurrentGame[position] = 0;
+        TotalRedPieces--;
+        GameHistoryUpdate("Red has " + TotalRedPieces + " pieces left");
+        // record lost pieces in game history
+    }
+    else if (CurrentGame[position] == 2 || CurrentGame[position] == 4) {
+        CurrentGame[position] = 0;
+        TotalWhitePieces--;
+        //record lost pieces in game history
+        GameHistoryUpdate("White has " + TotalWhitePieces + " pieces left");
+    }
+    if (TotalRedPieces == 0 || TotalWhitePieces == 0) {
+        GameHistoryUpdate("Game Over!");
+        if (TotalRedPieces == 0) {
+            GameHistoryUpdate("White Team Wins! <br>");
+        }
+        else if (TotalWhitePieces == 0) {
+            GameHistoryUpdate(" Team Wins!");
+        }
+        // Game over display who won.
+        //display winner in game history, dont allow any more moves.
+    }
 }
+
+var RedPlayerName = "Red Player";
+var WhitePlayerName = "White Player;"
 
 // this should be part of the view as well, the game hisroty is visible, 
 function GameHistoryDebug(){
@@ -386,10 +399,21 @@ function GameHistoryDebug(){
     //debuginfo.innerHTML= "Test";
 }
 function GameHistory(row, col){
+    //var paragraph = document.getElementById("gameHistory");
+    //paragraph.innerHTML="Selected = row: " + row + " col: " + col;
+    var temp = "Row: " + row;
+    temp += " Col: " + col;
+    GameHistoryUpdate("Selected = " + temp);
+}
+function GameHistoryUpdate(message)
+{
     var paragraph = document.getElementById("gameHistory");
-    paragraph.innerHTML="Selected = row: " + row + " col: " + col;
+    var temp = paragraph.innerHTML;
+    message += "<br>" + temp;
+    paragraph.innerHTML = message;
 }
 
+//move the the view.
 var Title = document.getElementById("TitleDiv");
 var value = 10;
 var colorSelect = 0;
@@ -398,7 +422,7 @@ function AnimateTitle(){
     setTimeout(function(){ clearInterval(interval); }, 10000);
     
 }
-
+//move to the view
 function moveTitle(){
         value += 5;
         Title.style.left = value + "px";
